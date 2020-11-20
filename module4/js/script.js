bd4c7b658cd9a7b8711585d8ad5460433f8b6cff
Alex.Thunder.UA <lxshvchnk@gmail.com>
2020-11-21 00:10:57 +0200

    JS practice

--- module4/js/script.js
+++ module4/js/script.js
@@ -28,6 +28,40 @@ console.log(myCircle.getArea());
 var myOtherCircle = new Circle(20);
 console.log(myOtherCircle);
 
+// Fun in Fin + this
+var literalCircle = {
+    radius: 10,
+  
+    getArea: function () {
+      var self = this;
+      console.log(this);
+  
+      var increaseRadius = function () {
+        //self.radius = 20;
+        this.radius = 20;
+      };
+      increaseRadius();
+      console.log(this.radius);
+  
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  };
+  
+  console.log(literalCircle.getArea());
+  console.log(literalCircle);
+  
+  
 
-
+  var circle = {
+    radius: 10,
+    initCircle: function (double) {
+      var increaseRadius = function () {
+        this.radius *= 2;
+      };
+      if (double)
+        increaseRadius();
+      return this.radius;
+    }
+  };
+  console.log(circle.initCircle("double"));
 
